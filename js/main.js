9d3d06a1ebb50fc69bde675f83b301c5e9c65374
EsmeRobledo <esmeralda.robledo@infosys.com>
2023-07-12 21:07:00 -0500

    cambio de texto a objeto

--- js/main.js
+++ js/main.js
@@ -2,9 +2,11 @@ console.log("Hola desde Js");
 
 const pedidoInput = document.getElementById("pedidoInput");
 const lista = document.getElementById("lista");
-const personaInput =document.getElementById("name")
-const telefonoInput=document.getElementById("phone")
-const direccionInput=document.getElementById("adress")
+const tablalista = document.getElementById("tablalista");
+
+const personaInput = document.getElementById("name")
+const telefonoInput = document.getElementById("phone")
+const direccionInput = document.getElementById("adress")
 
 
 let tareas = localStorage.getItem("tareas") ? JSON.parse(localStorage.getItem("tareas")) : [];
@@ -14,8 +16,8 @@ let telefonos = localStorage.getItem("telefonos") ? JSON.parse(localStorage.getI
 
 let editando = false;
 let nombre_previo = "";
- //personas = [ persona = {nombre : "", edad: ""}]
- //Esta condicional es igual a la condicional expuesta en tareas 
+//personas = [ persona = {nombre : "", edad: ""}]
+//Esta condicional es igual a la condicional expuesta en tareas 
 // if(localStorage.getItem("tareas")){
 //     let tareas = JSON.parse(localStorage.getItem("tareas"))
 //  }else{
@@ -33,91 +35,105 @@ let nombre_previo = "";
 
 
 function agregarTarea() {
-   if(editando){
-        tareas = tareas.map(tnueva => tnueva === nombre_previo ? pedidoInput.value : tnueva);
+    if (editando) {
+        console.log(nombre_previo);
+        tareas = tareas.map(tnueva => {
+            if (tnueva.nombre === nombre_previo) {
+                tnueva.nombre = pedidoInput.value
+                tnueva.cantidad = personaInput.value
+            }
+            return tnueva;
+        });
+
         localStorage.setItem("tareas", JSON.stringify(tareas));
         editando = false;
-   }else{
-    const tarea = pedidoInput.value;
-    tareas.push(tarea)
-    localStorage.setItem("tareas",JSON.stringify(tareas));
-   }
+    } else {
+        const tarea = { nombre: pedidoInput.value, cantidad: personaInput.value };
+        tareas.push(tarea)
+        localStorage.setItem("tareas", JSON.stringify(tareas));
+    }
     pedidoInput.value = "";
     actualizarLista();
 }
 
-function actualizarLista(){
+function actualizarLista() {
     lista.innerHTML = "";
+    tablalista.innerHTML = "";
     tareas.forEach(tarea => {
-        const li = document.createElement("li");
-        li.textContent = tarea;
-        li.classList.add("list-group-item");
+        // const li = document.createElement("li");
+        const tr = document.createElement("tr");
+        const tdNombre = document.createElement("td");
+        const tdcantidad = document.createElement("td");
+        tdNombre.textContent = tarea.nombre;
+        tr.appendChild(tdNombre)
+        tdcantidad.textContent = tarea.cantidad;
+        tr.appendChild(tdcantidad)
+        //li.classList.add("list-group-item");
 
         const btnEliminar = document.createElement("button");
         const iconoEliminar = document.createElement("i");
         iconoEliminar.classList.add("fa", "fa-trash");
         btnEliminar.appendChild(iconoEliminar);
         btnEliminar.classList.add("btn", "btn-danger", "float-right");
-        btnEliminar.addEventListener("click", () => deleteTarea(tarea));
-        li.appendChild(btnEliminar);
+        btnEliminar.addEventListener("click", () => deleteTarea(tarea.nombre));
+        tr.appendChild(btnEliminar);
 
         const btnEditar = document.createElement("button");
-         const iconEdit = document.createElement("i");
-         iconEdit.classList.add("fa", "fa-edit");
-         btnEditar.appendChild(iconEdit);
-         btnEditar.classList.add("btn", "btn-warning", "float-right");
-         btnEditar.addEventListener("click", () => editarTarea(tarea));
-         li.appendChild(btnEditar);
+        const iconEdit = document.createElement("i");
+        iconEdit.classList.add("fa", "fa-edit");
+        btnEditar.appendChild(iconEdit);
+        btnEditar.classList.add("btn", "btn-warning", "float-right");
+        btnEditar.addEventListener("click", () => editarTarea(tarea));
+        tr.appendChild(btnEditar);
 
-         lista.appendChild(li);
+        tablalista.appendChild(tr);
     });
 }
 
-function deleteTarea(tarea){
-    console.log(tarea);
-    tareas = tareas.filter(elemento => elemento !== tarea);
-    localStorage.setItem("tareas",JSON.stringify(tareas))
+function deleteTarea(tarea) {
+    tareas = tareas.filter(elemento => elemento.nombre !== tarea);
+    localStorage.setItem("tareas", JSON.stringify(tareas))
     actualizarLista();
 }
 
-function editarTarea(tarea){
-
-    editando=true
-    nombre_previo=tarea
-    pedidoInput.value = tarea;
-  
+function editarTarea(tarea) {
+    editando = true
+    nombre_previo = tarea.nombre
+    pedidoInput.value = tarea.nombre;
+    personaInput.value = tarea.cantidad
+    console.log(nombre_previo);
 }
 
 
-function limpiarStorage(){
+function limpiarStorage() {
     localStorage.clear();
     tareas = [];
     actualizarLista();
 }
 
-function enviarFormulario(){
+function enviarFormulario() {
 
     const nombre = personaInput.value;
     nombres.push(nombre);
-    localStorage.setItem("nombres",JSON.stringify(nombres));
-    
+    localStorage.setItem("nombres", JSON.stringify(nombres));
+
     const telefono = telefonoInput.value;
     telefonos.push(telefono);
-    localStorage.setItem("telefonos",JSON.stringify(telefonos));
-   
+    localStorage.setItem("telefonos", JSON.stringify(telefonos));
+
     const direccion = direccionInput.value;
     direcciones.push(direccion);
-    localStorage.setItem("direcciones",JSON.stringify(direcciones));
-    
-    
-    personaInput.value ="";
-    telefonoInput.value="";
-    direccionInput.value="";
+    localStorage.setItem("direcciones", JSON.stringify(direcciones));
 
 
-    const formulario=document.getElementById("contactForm");
-    const mensajeConfirmacion=document.createElement("p");
-    mensajeConfirmacion.textContent="Mensaje enviado";
+    personaInput.value = "";
+    telefonoInput.value = "";
+    direccionInput.value = "";
+
+
+    const formulario = document.getElementById("contactForm");
+    const mensajeConfirmacion = document.createElement("p");
+    mensajeConfirmacion.textContent = "Mensaje enviado";
     formulario.appendChild(mensajeConfirmacion);
 }
 
